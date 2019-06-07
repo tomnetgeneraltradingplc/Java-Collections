d341d5261fd8c38ea9bf3f6b38c12541b757c279
tomnetgeneraltradingplc <tomnetgeneraltradingplc@gmail.com>
2019-06-06 22:17:50 -0500

    Sorting ArrayList<Object> multiple properties with Comparator

--- src/ArrayList/Student.java
+++ src/ArrayList/Student.java
@@ -1,5 +1,7 @@
 package ArrayList;
 
+import java.util.Comparator;
+
 /**
  * Created by Thomas Tibebu,
  * Date May 20, 2019
@@ -50,6 +52,35 @@ public class Student implements Comparable<Student> {
         //return compareage-this.studentage;
     }
 
+//=================================================
+    //This under code use for Comparator
+/*Comparator for sorting the list by Student Name*/
+public static Comparator<Student> StuNameComparator = new Comparator<Student>() {
 
+    public int compare(Student s1, Student s2) {
+        String StudentName1 = s1.getStudentname().toUpperCase();
+        String StudentName2 = s2.getStudentname().toUpperCase();
+
+        //ascending order
+        return StudentName1.compareTo(StudentName2);
+
+        //descending order
+        //return StudentName2.compareTo(StudentName1);
+    }};
+
+    /*Comparator for sorting the list by roll no*/
+    public static Comparator<Student> StuRollno = new Comparator<Student>() {
+
+        public int compare(Student s1, Student s2) {
+
+            int rollno1 = s1.getRollno();
+            int rollno2 = s2.getRollno();
+
+            /*For ascending order*/
+            return rollno1-rollno2;
+
+            /*For descending order*/
+            //rollno2-rollno1;
+        }};
 
 }
