c630a214ffc66e1db3850e8eb38ff690bee1a39e
tomnetgeneraltradingplc <tomnetgeneraltradingplc@gmail.com>
2019-06-06 21:24:57 -0500

    Sort ArrayList in Ascending and Descending order

--- src/ArrayList/SortArrayList.java
+++ src/ArrayList/SortArrayList.java
@@ -27,6 +27,25 @@ public class SortArrayList {
         System.out.println("After Sorting:");
         for(String counter: listofcountries){
             System.out.println(counter);
+        }ArrayList<Integer> arraylist = new ArrayList<Integer>();
+        arraylist.add(11);
+        arraylist.add(2);
+        arraylist.add(7);
+        arraylist.add(3);
+        /* ArrayList before the sorting*/
+        System.out.println("Before Sorting:");
+        for(int counter: arraylist){
+            System.out.println(counter);
+        }
+
+        /* Sorting of arraylist using Collections.sort*/
+        Collections.sort(arraylist);
+
+        /* ArrayList after sorting*/
+        System.out.println("=============================");
+        System.out.println("After Sorting:");
+        for(int counter: arraylist){
+            System.out.println(counter);
         }
     }
 }
